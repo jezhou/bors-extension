6f418cf1dcfceea1eb90d55ccc7adb5572e246b2
Jesse Zhou <jesse.zhou@gusto.com>
2019-12-14 17:18:41 -0800

    eslint + babel + octokit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module"
+  },
+  "env": {
+    "es6": true
+  },
+  "parser": "babel-eslint",
+  "extends": "airbnb-base",
+};
--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'extension_dist/dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
